7bf5090d9847edb990e346a42d074d78b0ac1477
DonXI <68387776+DonXI@users.noreply.github.com>
2022-04-21 15:37:22 +0700

    Add files via upload

---
+++ test_ir.ino
@@ -0,0 +1,28 @@
+#define buzzer 13
+#define ir_analog 32
+const int ch = 0; // channel
+const int bpm = 120;
+const int quarter_note_delay = (60 * 1000) / bpm;
+
+void setup() {
+  Serial.begin(115200);
+  //set timer
+  ledcSetup(ch, 0, 8); 
+  ledcAttachPin(buzzer, ch);
+}
+
+void loop() {
+  int val_ir = analogRead(ir_analog);
+  ledcWrite(ch,25);  // set volume duty cycle = 25%
+  Serial.println(val_ir);
+  
+  if(val_ir > 0){
+    ledcWrite(ch,0);
+  }
+  else{
+    ledcWriteNote(ch, NOTE_C, 4);
+    delay(quarter_note_delay);
+    
+  }
+  delay(100);
+}
