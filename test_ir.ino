f628d716ab71c2cb80782da9672f38e1dee32ee1
DonXI <68387776+DonXI@users.noreply.github.com>
2022-04-21 15:39:52 +0700

    Update test_ir.ino

--- test_ir.ino
+++ test_ir.ino
@@ -1,6 +1,6 @@
-#define buzzer 13
-#define ir_analog 32
-const int ch = 0; // channel
+#define buzzer 13     // pin buzzer
+#define ir_analog 32  // pin IR 
+const int ch = 0;     // channel
 const int bpm = 120;
 const int quarter_note_delay = (60 * 1000) / bpm;
 
@@ -15,14 +15,13 @@ void loop() {
   int val_ir = analogRead(ir_analog);
   ledcWrite(ch,25);  // set volume duty cycle = 25%
   Serial.println(val_ir);
-  
+  // IR detect 
   if(val_ir > 0){
     ledcWrite(ch,0);
   }
   else{
     ledcWriteNote(ch, NOTE_C, 4);
     delay(quarter_note_delay);
-    
   }
   delay(100);
 }
